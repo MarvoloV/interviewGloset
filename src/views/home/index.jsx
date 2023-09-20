1755525783a181bea7ca220a82ac94a33d4b3cdb
Jorge Luis Adco Ccahuay <jorgeluis@Jorges-Mac-Pro.local>
2023-09-19 19:28:09 -0500

    update code

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,31 +1,48 @@
-import { ArrowRightOutlined } from '@ant-design/icons';
-import { MessageDisplay } from '@/components/common';
-import { ProductShowcaseGrid } from '@/components/product';
-import { FEATURED_PRODUCTS, RECOMMENDED_PRODUCTS, SHOP } from '@/constants/routes';
+import { ArrowRightOutlined } from "@ant-design/icons";
+import { MessageDisplay } from "@/components/common";
+import { ProductShowcaseGrid } from "@/components/product";
 import {
-  useDocumentTitle, useFeaturedProducts, useRecommendedProducts, useScrollTop
-} from '@/hooks';
-import bannerImg from '@/images/banner-girl.png';
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+  FEATURED_PRODUCTS,
+  RECOMMENDED_PRODUCTS,
+  SHOP,
+} from "@/constants/routes";
+import {
+  useDocumentTitle,
+  useFeaturedProducts,
+  useRecommendedProducts,
+  useScrollTop,
+} from "@/hooks";
+import bannerImg from "@/images/banner-girl.png";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 const Home = () => {
-  useDocumentTitle('Salinaka | Home');
+  useDocumentTitle("Salinaka | Home");
   useScrollTop();
+  const history = useHistory();
+  const { user } = useSelector((state) => ({
+    user: state.auth,
+  }));
 
   const {
     featuredProducts,
     fetchFeaturedProducts,
     isLoading: isLoadingFeatured,
-    error: errorFeatured
+    error: errorFeatured,
   } = useFeaturedProducts(6);
   const {
     recommendedProducts,
     fetchRecommendedProducts,
     isLoading: isLoadingRecommended,
-    error: errorRecommended
+    error: errorRecommended,
   } = useRecommendedProducts(6);
+  useEffect(() => {
+    if (user) {
+      history.push("/admin/dashboard");
+    }
+  }, [user]);
 
   return (
     <main className="content">
@@ -38,8 +55,9 @@ const Home = () => {
               <strong>Clarity</strong>
             </h1>
             <p>
-              Buying eyewear should leave you happy and good-looking, with money in your pocket.
-              Glasses, sunglasses, and contacts—we’ve got your eyes covered.
+              Buying eyewear should leave you happy and good-looking, with money
+              in your pocket. Glasses, sunglasses, and contacts—we’ve got your
+              eyes covered.
             </p>
             <br />
             <Link to={SHOP} className="button">
@@ -47,14 +65,16 @@ const Home = () => {
               <ArrowRightOutlined />
             </Link>
           </div>
-          <div className="banner-img"><img src={bannerImg} alt="" /></div>
+          <div className="banner-img">
+            <img src={bannerImg} alt="" />
+          </div>
         </div>
         <div className="display">
           <div className="display-header">
             <h1>Featured Products</h1>
             <Link to={FEATURED_PRODUCTS}>See All</Link>
           </div>
-          {(errorFeatured && !isLoadingFeatured) ? (
+          {errorFeatured && !isLoadingFeatured ? (
             <MessageDisplay
               message={errorFeatured}
               action={fetchFeaturedProducts}
@@ -72,7 +92,7 @@ const Home = () => {
             <h1>Recommended Products</h1>
             <Link to={RECOMMENDED_PRODUCTS}>See All</Link>
           </div>
-          {(errorRecommended && !isLoadingRecommended) ? (
+          {errorRecommended && !isLoadingRecommended ? (
             <MessageDisplay
               message={errorRecommended}
               action={fetchRecommendedProducts}
